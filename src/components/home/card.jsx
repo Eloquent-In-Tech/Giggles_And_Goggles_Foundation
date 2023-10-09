4ec02d284bbb6ad2f0243dea25f0ef543171d2f3
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-10-09 16:44:34 +0100

    corrections

--- src/components/home/card.jsx
+++ src/components/home/card.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 function Card() {
   return (
     <div className='flex flex-col-4 space-x-[30px] pt-[20px] ml-[50px] mb-[100px]'>
-        <div className='h-[400px] w-[250px] bg-gray-300'>
+        <div className='h-[300px] w-[250px] bg-gray-300'>
 
-            <h5 className='mt-[400px] bg-blue-400 py-3'>VIEW</h5>
+            <h5 className='mt-[300px] bg-blue-500 py-3 hover:bg-blue-300 hover:cursor-pointer'>VIEW</h5>
         </div>
-        <div className='h-[400px] w-[250px] bg-gray-300'>
+        <div className='h-[300px] w-[250px] bg-gray-300'>
 
-            <h5 className='mt-[400px] bg-blue-400 py-3'>VIEW</h5>
+            <h5 className='mt-[300px] bg-blue-500 py-3 hover:bg-blue-300 hover:cursor-pointer'>VIEW</h5>
         </div>
-        <div className='h-[400px] w-[250px] bg-gray-300'>
+        <div className='h-[300px] w-[250px] bg-gray-300'>
 
-            <h5 className='mt-[400px] bg-blue-400 py-3'>VIEW</h5>
+            <h5 className='mt-[300px] bg-blue-500 py-3 hover:bg-blue-300 hover:cursor-pointer'>VIEW</h5>
         </div>
-        <div className='h-[400px] w-[250px] bg-gray-300'>
+        <div className='h-[300px] w-[250px] bg-gray-300 '>
 
-            <h5 className='mt-[400px] bg-blue-400 py-3'>VIEW</h5>
+            <h5 className='mt-[300px] bg-blue-500 py-3 hover:bg-blue-300 hover:cursor-pointer'>VIEW</h5>
         </div>
     </div>
   )
