cd465cc2ff298d0a8d5d052146704ed990a98fd6
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-10-09 15:30:08 +0100

    Hero Section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import TopNav from '../components/topNav'
+import TopNav from '../components/general/topNav'
+import HomeHero from '../components/home/Hero'
+
 
 function Home() {
   return (
     <div>
         <TopNav/>
+        <HomeHero/>
     </div>
   )
 }
