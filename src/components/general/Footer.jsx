ca74317d7ed10459218b75b4245eaf2b22719fcd
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-10-11 13:14:48 +0100

    footer done!

--- src/components/general/Footer.jsx
+++ src/components/general/Footer.jsx
@@ -1,22 +1,71 @@
 import { footerData } from "../../data/FooterData"
+import { footerAboutDetails } from "../../data/FooterData"
+import { footerEventsDetails } from "../../data/FooterData"
+import logo from "../../assets/hurlingham_logo.png"
+
+
 function Footer() {
     return (
-        <div>
-            <div className=' h-[600px] w-full bg-slate-300'>
+    
+        <div className="h-[600px]  w-full bg-slate-300">
+            <div className="h-[150px] w-[150px] bg-gray-100 rounded-full absolute top-[1350px] left-[600px]">
+                <img className=" bottom-[5px] left-[200px] w-[250px]  p-[20px]" src={logo} alt="logo" />
+            </div>
+            <div className='flex flex-col-4 space-x-[180px]  text-gray-500 '>
                 {footerData.map((data, index)=>{
               return(
-                    // <div key={index} className="flex flex-row">
-                        
-                    //         <h1>{data.title}</h1>
-                    // </div>
-                    <div key={index} className="flex flex-row">
-                    {data.title.split(', ').map((item, itemIndex) => (
-                        <h1 key={itemIndex} className={itemIndex !== 0 ? 'ml-2' : ''}>
-                        {item}
-                        </h1>
-  ))}
-</div>
+                    <div key={index} className="ml-[155px] mt-[100px] font-bold ">
+                        <h1>{data.title}</h1>
+                    </div>
+                    
                 )})}
+            </div>
+            {/* <div className="flex flex-row">
+                <div className="mr-[1000px] space-y-[30px] mt-[30px] text-gray-500">
+                        {footerAboutDetails.map((data, index)=>{
+                            return(
+                                <div key={index}>
+                                    <a href={data.link}>
+                                    <ul>{data.title}</ul>
+                                    </a>
+                                </div>
+                            )
+                        })}
+                   
+                </div>
+                <div className=" mt-[30px] text-gray-500">
+                    {footerEventsDetails.map((data, index)=>{
+                        return(
+                            <div key={index}>
+                                <a href={data.link}>
+                                    <ul>{data.title}</ul>
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div> */}
+            <div className="flex flex-row space-x-[35px] text-gray-600 mt-[30px] ml-[20px] ">
+                <div className="space-x-[5px] ml-[35px] ">
+                    <a href="#aboutHurlingham"><ul className="hover:text-blue-400 hover:cursor-pointer hover:underline">ABOUT HURLINGHAM</ul></a>
+                    <a href="#sustainability"><ul className="hover:text-blue-400 hover:cursor-pointer hover:underline">SUSTAINABILITY</ul></a>
+                    <a href="#safeguarding"><ul className="hover:text-blue-400 hover:cursor-pointer hover:underline">SAFEGUARDING</ul></a>
+                    <a href="#thehurlinghamclubfoundation"><ul className="hover:text-blue-400 hover:cursor-pointer hover:underline">THE HURLINGHAM CLUB FOUNDATION</ul></a>
+                </div>
+                <div className="">
+                    <a href="#privateEvents"><ul className="hover:text-blue-400 hover:cursor-pointer hover:underline">PRIVATE EVENTS</ul></a>
+                </div>
+                <div className="ml-[90px] space-x-[-5px]">
+                    <a href="#contactTheClub"><ul className="ml-[85px] hover:text-blue-400 hover:cursor-pointer hover:underline">CONTACT THE CLUB</ul></a>
+                    <a href="#map&directions"><ul className="ml-[85px] hover:text-blue-400 hover:cursor-pointer hover:underline">MAP & DIRECTIONS</ul></a>
+                    <a href="#instagram"><ul className="ml-[85px] hover:text-blue-400 hover:cursor-pointer hover:underline">INSTAGRAM</ul></a>
+                </div>
+                <div>
+                    <a href="#opportunities"><ul className="ml-[90px] hover:text-blue-400 hover:cursor-pointer hover:underline">OPPORTUNITIES</ul></a>
+                </div>
+                <div>
+                    <a href="#termsOfUse"><ul className="ml-[105px] hover:text-blue-400 hover:cursor-pointer hover:underline">TERMS OF USE</ul></a>
+                </div>
             </div>
       </div>
 
--- src/data/FooterData.jsx
+++ src/data/FooterData.jsx
@@ -1,6 +1,7 @@
 export const footerData = [
     {
         title: "ABOUT",
+
     },
     {
         title: "EVENTS",
@@ -14,5 +15,62 @@ export const footerData = [
     },
     {
         title: "LEGAL",
+    },
+]
+export const footerAboutDetails = [
+    {
+        title: "ABOUT HURLINGHAM",
+        link: "#aboutHurlingham",
+    },
+    {
+        title: "SUSTAINABILITY",
+        link: "#sustainability"
+
+    },
+    {
+        title: "SAFEGUARDING",
+        link: "#safeguarding"
+        
+    },
+    {
+        title: "THE HURLINGHAM CLUB FOUNDATION",
+        link: "#thehurlinghamclubfoundation"
+    },
+    
+]
+export const footerEventsDetails = [
+    {
+        title: "PRIVATE EVENTS",
+        link: "#privateEvents",
+    }
+    
+]
+export const footerContactDetails = [
+    {
+        title: "CONTACT THE CLUB",
+        link: "#contactTheClub",
+    },
+    {
+        title: "MAP & DIRECTIONS",
+        link: "#map&directions"
+
+    },
+    {
+        title: "INSTAGRAM",
+        link: "#instagram"
+        
+    },
+    
+]
+export const footerCareerDetails = [
+    {
+        title: "OPPORTUNITIES",
+        link: "#opportunities",
+    },
+]
+export const footerLegalDetails = [
+    {
+        title: "TERMS OF USE",
+        link: "#termsOfUse",
     },
 ]
