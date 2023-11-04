a5b743ce416731990cd14ad35ad1e5a6f254ef8a
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-11-04 17:20:32 +0100

    Giggles and goggles!

--- src/components/hurlingham_foundation/FoundationNavBar.jsx
+++ src/components/hurlingham_foundation/FoundationNavBar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function FoundationNavBar() {
   return (
     <div className='bg-gray-50 h-[80px]'>
-        <h1 className='pt-[15px] pr-[500px] text-gray-600'>
+        <h1 className='pt-[15px] pr-[400px] text-gray-600'>
             <a href="/"><span className='mr-[10px] hover:underline hover:cursor-pointer'>HOME</span></a>
-            <a href="/"><span className='mr-[10px] hover:underline hover:cursor-pointer'>  / HURLINGHAM HOME</span></a> 
-            <a href="/about"><span className='mr-[10px] hover:underline hover:cursor-pointer'>   / ABOUT HURLINGHAM</span></a>
-            <span className='text-gray-400'> / THE HURLINGHAM CLUB FOUNDATION </span>
+            <a href="/"><span className='mr-[10px] hover:underline hover:cursor-pointer'>  / GIGGLES AND GOGGLES HOME</span></a> 
+            <a href="/about"><span className='mr-[10px] hover:underline hover:cursor-pointer'>   / ABOUT GIGGLES AND GOGGLES</span></a>
+            <span className='text-gray-400'> / THE GIGGLES AND GOGGLES FOUNDATION </span>
               
         </h1>
     </div>
--- src/components/hurlingham_foundation/HeroFoundation.jsx
+++ src/components/hurlingham_foundation/HeroFoundation.jsx
@@ -10,9 +10,9 @@ function HeroFoundation() {
             <img src={hero} alt="..." className='h-[300px] ml-[530px] mt-[100px] mb-[40px]'/>
         </div>
         <div>
-            <h3 className=' text-4xl text-gray-600 mb-[10px]'>THE HURLINGHAM CLUB FOUNDATION</h3>
+            <h3 className=' text-4xl text-gray-600 mb-[10px]'>THE GIGGLES AND GOGGLES FOUNDATION</h3>
             <h4 className='text-gray-600 mb-[50px]'> 
-            In 2021The Hurlingham Club Foundation was established with support from staff, 
+            In 2021 The Giggles and Goggles Foundation was established with support from staff, 
             members and the local community. <br /> <br />
             We have already set up meaningful partnerships with local schools and charities in order to 
             bring about positive change and help <br />many families, children and elderly neighbours to thrive. <br /> 
