19d598369a24fd07ecc5654213c7fcf33089980e
NwachukwuSam <nwachukwusamuel123@gmail.com>
2023-11-06 13:55:40 +0100

    Logo  Changes!

--- src/components/corporate/CorporateNavBar.jsx
+++ src/components/corporate/CorporateNavBar.jsx
@@ -5,7 +5,7 @@ function CorporateNavBar() {
     <div className='bg-gray-50 h-[80px]'>
         <h1 className='pt-[15px] pr-[600px] text-gray-600'>
             <a href="/"><span className='mr-[10px] hover:underline hover:cursor-pointer'>HOME</span></a>
-            <a href="/"><span className='mr-[10px] hover:underline hover:cursor-pointer'>  / GIGGLES AND GOGGLES HOME</span></a> 
+            <a href="/"><span className='mr-[10px] hover:underline hover:cursor-pointer'>  / GOGGLE AND GIGGLES HOME</span></a> 
             <a href="/events"><span className='mr-[10px] hover:underline hover:cursor-pointer'>  / PRIVATE EVENTS</span></a>
             <span className='text-gray-400'> / CORPORATE, CHARITY & AWARDS </span>
               
--- src/components/corporate/CorporatePhotoPanel.jsx
+++ src/components/corporate/CorporatePhotoPanel.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import photo from "../../assets/corporate/corporatePhotoes.png"
-import logo from '../../assets/Hurlingham_logo.png'
+import logo from '../../assets/giggles_logo.png'
 
 function PhotoPanel() {
   return (
     <div className='flex justify-center mb-[200px]'>
         <img src={photo} alt="" className='w-[980px]  ' />
 
-        <div className="h-[140px] w-[140px] bg-gray-100 rounded-full absolute top-[3485px] left-[600px]">
-            <img className=" bottom-[5px] left-[200px] w-[250px]  p-[20px]" src={logo} alt="logo" />
+        <div className="h-[140px] w-[140px] bg-white border-[2px] rounded-full absolute top-[3585px] left-[600px]">
+            <img className=" bottom-[5px] left-[200px] w-[750px] h-[150px]  p-[20px]" src={logo} alt="logo" />
         </div>
     </div>
   )
